2448b490bdd38aac30949d3f82f01be4e232e7da
Stefanus Deny <maxoxygenless@gmail.com>
2025-12-31 14:30:50 +0700

    Pembulatan jarak ke kilometer
    kalau luas/jarak di Area Measure melebihi 999.999 m, dibulatkan menjadi km.

--- Assets/Script/ButtonCustom.cs
+++ Assets/Script/ButtonCustom.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 // =========================================
 // Script untuk toggle show/hide objek UI
@@ -6,15 +7,29 @@ using UnityEngine;
 public class ButtonCustom : MonoBehaviour
 {
     // Objek yang akan di-toggle (drag dari Inspector)
-    [SerializeField] 
+    [SerializeField]
     GameObject target;
 
+    // Referensi ke Toggle UI (drag dari Inspector)
+    [SerializeField]
+    Toggle toggleComponent;
+
     // Panggil fungsi ini untuk toggle show/hide
-    public void Toggle()
+    public void ToggleVisibility()
     {
         if (target != null)
         {
             target.SetActive(!target.activeSelf);
         }
+    }
+
+    public void ToggleColor()
+    {
+        Image img = target.GetComponent<Image>();
+
+        if (toggleComponent.isOn)
+            img.color = new Color32(0x36, 0xB7, 0x68, 255); // #36B768
+        else
+            img.color = new Color32(0xFF, 0xFF, 0xFF, 255); // #FFFFFF
     }
 }
